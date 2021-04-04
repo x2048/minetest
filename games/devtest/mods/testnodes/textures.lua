917ddcd5e7df448b598dbe93400746fbbe4455da
Dmitry Kostenko <codeforsmile@gmail.com>
2021-04-04 17:15:17 +0200

    Fix order of tiles in liquid sources
    Add multi-textured test liquid source node

--- games/devtest/mods/testnodes/textures.lua
+++ games/devtest/mods/testnodes/textures.lua
@@ -80,3 +80,22 @@ for a=1,#alphas do
 	})
 end
 
+-- Colored water
+minetest.register_node("testnodes:multitile_liquid", {
+	description = S("Multiple Tiles Liquid Test Node"),
+	drawtype = "liquid",
+	paramtype = "light",
+	tiles = {
+		"default_water.png^[colorize:#ff00007f^[opacity:192",
+		"default_water.png^[colorize:#00ffff7f^[opacity:192",
+		"default_water.png^[colorize:#0000ff7f^[opacity:192",
+		"default_water.png^[colorize:#ffff007f^[opacity:192",
+		"default_water.png^[colorize:#00ff007f^[opacity:192",
+		"default_water.png^[colorize:#ff00ff7f^[opacity:192",
+	},
+	inventory_image = "default_water.png^[colorize:#00ff007f",
+	wield_image = "default_water.png^[colorize:#00ff007f",
+	use_texture_alpha = "blend",
+
+	groups = { dig_immediate = 3 },
+})
