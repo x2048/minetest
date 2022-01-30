0c8873629d26326d265b6063aef15b83f9e92a85
Dmitry Kostenko <codeforsmile@gmail.com>
2022-01-30 13:56:51 +0100

    Separate command for tint in the test mod

--- mods/lighting/init.lua
+++ mods/lighting/init.lua
@@ -8,8 +8,14 @@ core.register_chatcommand("b", {
 	params = "brightness",
 	func = function(name, param)
 		local player = core.get_player_by_name(name)
-		local brightness = tonumber(param)
-		-- player:set_lighting({ brightness = brightness, color_tint = {r = 255, g = 255, b = 255 }})
-		player:set_lighting({ brightness = brightness, color_tint = {r = 255, g = 179 * (1 + brightness), b = 128 * (1 + brightness)}})
+		player:set_lighting({ brightness = tonumber(param) })
+	end
+})
+
+core.register_chatcommand("t", {
+	params = "color",
+	func = function(name, param)
+		local player = core.get_player_by_name(name)
+		player:set_lighting({ color_tint = param })
 	end
 })
