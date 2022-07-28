c2e6c3e6603adae0b75d1998a5919cdb3e92077a
x2048 <codeforsmile@gmail.com>
2022-07-28 14:54:21 +0200

    Pass sun/moon brightness to the shaders

--- src/client/game.cpp
+++ src/client/game.cpp
@@ -402,7 +402,9 @@ class GameGlobalShaderConstantSetter : public IShaderConstantSetter
 	float m_bloom_radius;
 	CachedPixelShaderSetting<float> m_saturation_pixel;
 	CachedPixelShaderSetting<float, 3> m_sun_position_pixel;
+	CachedPixelShaderSetting<float> m_sun_brightness_pixel;
 	CachedPixelShaderSetting<float, 3> m_moon_position_pixel;
+	CachedPixelShaderSetting<float> m_moon_brightness_pixel;
 
 public:
 	void onSettingsChange(const std::string &name)
@@ -462,7 +464,9 @@ public:
 		m_bloom_radius_pixel("bloomRadius"),
 		m_saturation_pixel("saturation"),
 		m_sun_position_pixel("sunPositionScreen"),
-		m_moon_position_pixel("moonPositionScreen")
+		m_sun_brightness_pixel("sunBrightness"),
+		m_moon_position_pixel("moonPositionScreen"),
+		m_moon_brightness_pixel("moonBrightness")
 	{
 		g_settings->registerChangedCallback("enable_fog", settingsCallback, this);
 		g_settings->registerChangedCallback("exposure_compensation", settingsCallback, this);
@@ -594,10 +598,16 @@ public:
 
 			float sun_position_array[3] = { sun_position.X, sun_position.Y, sun_position.Z};
 			m_sun_position_pixel.set(sun_position_array, services);
+
+			float sun_brightness = MYMAX(0.f, daynight_ratio / 1000.f - 1. + 0.75) / 0.75;
+			m_sun_brightness_pixel.set(&sun_brightness, services);
 		}
 		else {
 			float sun_position_array[3] = { 0.f, 0.f, -1.f };
 			m_sun_position_pixel.set(sun_position_array, services);
+
+			float sun_brightness = 0.f;
+			m_sun_brightness_pixel.set(&sun_brightness, services);
 		}
 
 		if (m_sky->getMoonVisible()) {
@@ -608,10 +618,16 @@ public:
 
 			float moon_position_array[3] = { moon_position.X, moon_position.Y, moon_position.Z};
 			m_moon_position_pixel.set(moon_position_array, services);
+
+			float moon_brightness = -MYMIN(0.f, daynight_ratio / 1000.f - 0.55) / 0.55;
+			m_moon_brightness_pixel.set(&moon_brightness, services);
 		}
 		else {
 			float moon_position_array[3] = { 0.f, 0.f, -1.f };
 			m_moon_position_pixel.set(moon_position_array, services);
+
+			float moon_brightness = 0.f;
+			m_moon_brightness_pixel.set(&moon_brightness, services);
 		}
 	}
 
